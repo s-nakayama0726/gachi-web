688ce1860851d7fad7724e120eff96319decc5e5
s-nakayama0726 <shingood98@gmail.com>
2018-04-22 02:38:28 +0900

    フロント埋め込み

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,13 @@
+class MainController < ApplicationController
+  def index
+    
+  end
+  
+  def event
+    
+  end
+  
+  def otaku
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "main#index"
+  
+  # main contents
+  get "/index" => "main#index"
+  get "/event" => "main#event"
+  get "/otaku" => "main#otaku"
 end
